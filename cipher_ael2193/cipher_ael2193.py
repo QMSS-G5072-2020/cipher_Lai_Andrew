39494a3cc7ede5539807513bbb2f484f6ebff7bd
Andrew <ael2193@columbia.edu>
2020-11-15 14:39:29 -0800

    Second Commit

--- cipher_ael2193/cipher_ael2193.py
+++ cipher_ael2193/cipher_ael2193.py
@@ -1,4 +1,24 @@
 def cipher(text, shift, encrypt=True):
+    """
+        Transform words into using cipher.
+    Parameters:
+    -----------
+    text = A string that is inputted into the function to be transformed
+    shift = An integer that indicates how many places down the alphabet the word is replace with
+    encrypt = A boolean value (True and False)
+        If True: text will be encrypted
+        If False: text will be decrypted
+        
+    Examples:
+    -----------
+    cipher("hello", 1)
+    "ifmmp"
+    cipher("hello", -1)
+    "gdkkn"
+    cipher("hello", 1, encrypt = False)
+    "ifmmp"
+    """
+
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
